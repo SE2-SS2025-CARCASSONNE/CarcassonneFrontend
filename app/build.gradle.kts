536b7c2e771d1229dbaa3dc5cc6a0b604fcecdce
thesalvenmoser <thesalvenmoser@gmail.com>
2025-04-02 19:30:38 +0200

    Add dependencies for Krossbow Websocket

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation("org.hildan.krossbow:krossbow-websocket-okhttp:7.0.0")
+    implementation("org.hildan.krossbow:krossbow-stomp-core:7.0.0")
+    implementation("org.hildan.krossbow:krossbow-websocket-builtin:7.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
